b985a4e62069552f0157ec24c2ebe55a8e46c96d
mr.Shu <mr@shu.io>
2014-11-29 18:26:03 +0100

    update: Added setup.py
    * Added setup.py to make installing the module simple.
    
    Signed-off-by: mr.Shu <mr@shu.io>

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from setuptools import setup
+
+setup(name='imhdsk',
+      version='0.0.1',
+      description='An unofficial API for imhd.sk',
+      maintainer='mr.Shu',
+      maintainer_email='mr@shu.io',
+      url='https://github.com/mrshu/python-imhdsk-api',
+      packages=['imhdsk'])
